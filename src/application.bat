b5dd9fdf6467ff2a93862bdbf9d9fc3f939da19e
Camille Lengrand <clengrand@gmail.com>
2015-03-17 23:00:59 +0100

    add var to script

--- src/application.bat
+++ src/application.bat
@@ -1,5 +1,6 @@
 ### application.bat - 2015 - Camille Lengrand ###
+version = "2"
+build_hour = "11H00"
 
-echo "Copyright company - 2015"
-echo "Build : 10:00"
-echo "version 1.1"
+echo "Build ${build_hour}"
+echo "version ${version}"
